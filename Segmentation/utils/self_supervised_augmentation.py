64bc34586093279126c341339d54616a666c4d46
arahosu <arahosu@gmail.com>
2020-06-05 16:02:50 +0200

    added a draft of self_supervised_augmentation.py

---
+++ Segmentation/utils/self_supervised_augmentation.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+class AutoAugment(object):
+
+    def __init__(self, sample):
+
+        # random flips
+
+        # random rotations
+
+        # random brightness/contrast
+
+        # random noise (Gaussian)
+
+        # random filter (sobel)
+
+        # random resolution
+
+        # V00 <--> V01 of the same knee
